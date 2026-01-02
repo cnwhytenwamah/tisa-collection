cef25946899b8ca8b1aecf73a7db927aff3ff1ef
cnwhytenwamah <cnwhytenwamah@gmail.com>
2026-01-02 13:53:16 +0100

    bug fixed

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
     <footer className="bg-[#3A3A3A] text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
-          <div className="col-span-1 md:col-span-2 h-10">
+          <div className="col-span-1 md:col-span-2 h-auto md:h-10">
             <ImageComponent
               src="/images/hero/logo.png"
               alt="Tisa Collections Logo"
